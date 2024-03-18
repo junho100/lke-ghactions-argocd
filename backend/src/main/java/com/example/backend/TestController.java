1c21f3461557a835a3e17a2c0e80587e58640d21
junho100 <bemodesty306@naver.com>
2024-03-18 23:16:41 +0900

    Test: cicd test

--- backend/src/main/java/com/example/backend/TestController.java
+++ backend/src/main/java/com/example/backend/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping
     public String test() {
-        return "this is v2";
+        return "this is v3";
     }
 }
